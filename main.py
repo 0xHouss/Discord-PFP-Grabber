4843593b3fc9c44b1c501b273a0bb2bc2bf4f764
JL Houss <tkthoussam@gmail.com>
2022-11-21 23:45:32 +0100

    v3

--- main.py
+++ main.py
@@ -6,25 +6,25 @@ from PIL import Image
 from urllib.request import Request, urlopen
 
 
-
 TOKEN = env['TOKEN']
 
 user_id = int(input("discord user id: "))
 
-username = requests.get(f'https://discord.com/api/v9/users/{user_id}', headers={"Authorization": f"Bot {TOKEN}"}).json()['username']
+req = requests.get(f'https://discord.com/api/v9/users/{user_id}', headers={"Authorization": f"Bot {TOKEN}"}).json()
 
-avatar_id = requests.get(f'https://discord.com/api/v9/users/{user_id}', headers={"Authorization": f"Bot {TOKEN}"}).json()['avatar']
+username = req['username']
+avatar_id = req['avatar']
 
-req = Request(f"https://cdn.discordapp.com/avatars/{user_id}/{avatar_id}.webp", headers={'User-Agent': 'Mozilla/5.0'})
+pfp_response = Request(f"https://cdn.discordapp.com/avatars/{user_id}/{avatar_id}.webp", headers={'User-Agent': 'Mozilla/5.0'})
 
-with urlopen(req) as u:
-    raw_data = u.read()
+with urlopen(pfp_response) as response:
+    pfp = response.read()
 
-pfp = Image.open(BytesIO(raw_data))
+pfp_image = Image.open(BytesIO(pfp))
 
 if not path.isdir("Profile Pictures"):
     makedirs("Profile Pictures\\")
 
-pfp.save(f"Profile Pictures\\{username}.png")
+pfp_image.save(f"Profile Pictures\\{username}.png")
 
 # pfp.save(f"/Profile Pictures/{username}.png")
